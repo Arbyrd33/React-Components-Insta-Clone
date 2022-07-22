5b8ca8b0698114e6fd45e1d4bea6b9089f7ee4f5
Arbor Matos <arbor@MacBook-Pro.local>
2022-07-21 19:42:41 -0700

    I am free of errors, time to run tests and see if I forgot anything

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -10,6 +10,9 @@ const Comments = props => {
     <div>
       {/* {console.log(comments.id) /*it's undefined... sigh*/}
       {/* map through the comments prop and render a Comment for every piece of data */}
+      {comments.map(comment=>{
+        return <Comment comment = {comment} key = {comment.id}/>
+      })}
     </div>
   );
 };
